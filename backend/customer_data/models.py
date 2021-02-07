809196715cb0018ad92483d4f0f0a66ba0203476
VishnuKrishnathu <vishnu.krishnathu1999@gmail.com>
2021-02-07 13:31:58 +0530

    project finished

--- backend/customer_data/models.py
+++ backend/customer_data/models.py
@@ -1,5 +1,6 @@
 from django.db import models 
 from django.core.validators import MinValueValidator
+import pymongo
 
 # Create your models here.
 class CustomerInfo(models.Model):
@@ -8,3 +9,9 @@ class CustomerInfo(models.Model):
     gender = models.CharField(max_length = 10)
     age = models.IntegerField( validators = [MinValueValidator(1)])
     account_balance = models.IntegerField( validators = [MinValueValidator(7000)])
+
+
+client = pymongo.MongoClient("mongodb+srv://vishnu:YHwvgPjn7XfDLxJL@cluster0.ragko.mongodb.net/mycustomers?retryWrites=true&w=majority")
+db = client["mycustomers"]
+collection = db["customers_data"]
+data= collection.find()
--- backend/customer_data/urls.py
+++ backend/customer_data/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('customers', views.customers),
+    path('customers', views.customers, name = 'customers'),
+    path('customers/<str:first_name>', views.customer_data, name = 'customer'),
+    path('customers/<str:first_name>/paymentdone', views.paymentdone , name = 'paymentdone'),
 ]
